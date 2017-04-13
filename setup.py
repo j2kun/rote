1c9ddcc54b97fed3a8e10f36aaffc31509afe134
Jeremy Kun <jeremy@21.co>
2017-04-13 15:06:48 -0700

    improve setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,25 @@
 from setuptools import setup
 
-setup(name='rote',
-      version='0.1',
-      description='A tiny python library for making human-in-the-loop terminal apps',
-      url='http://github.com/j2kun/rote',
-      author='Jeremy Kun',
-      author_email='mathintersectprogramming@gmail.com',
-      license='MIT',
-      packages=['rote'],
-      zip_safe=False)
+setup(
+    name='rote',
+    version='0.0.1',
+    description='A tiny python library for making human-in-the-loop terminal apps',
+    url='http://github.com/j2kun/rote',
+    author='Jeremy Kun',
+    author_email='mathintersectprogramming@gmail.com',
+    license='MIT',
+    packages=['rote'],
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Automation Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+    keywords='rote human labeling',
+
+)
