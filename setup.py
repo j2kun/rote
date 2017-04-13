600de6613bd3283b8f90c552651150ccd9cda29f
Jeremy Kun <jeremy@21.co>
2017-04-13 14:13:19 -0700

    rote: add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='rote',
+      version='0.1',
+      description='A tiny python library for making human-in-the-loop terminal apps',
+      url='http://github.com/j2kun/rote',
+      author='Jeremy Kun',
+      license='MIT',
+      packages=['rote'],
+      zip_safe=False)
