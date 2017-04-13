b9b3bd60328452e2f393a8f756e0fcc3eee6ae35
Jeremy Kun <jeremy@21.co>
2017-04-13 14:09:09 -0700

    move to class-based format

--- rote.py
+++ rote.py
@@ -1,102 +1,95 @@
-
-handlers = {
-    'setup': None,
-    'newdata': None,
-    'foreach': None,
-    'skipif': None,
-    'describe': None,
-    'teardown': None,
-}
+import sys
 
 
-def wrap_in_try(f, with_teardown=True, msg=None):
-    def wrapped(*args, **kwargs):
-        if with_teardown:
+class Rote(object):
+    def __init__(self):
+        self.handlers = {
+            'setup': None,
+            'newdata': None,
+            'foreach': None,
+            'skipif': None,
+            'describe': None,
+            'teardown': None,
+        }
+
+        self.accumulator = []
+        self.new_data = []
+
+    def wrap_in_try(self, f, with_teardown=True):
+        def wrapped(*args, **kwargs):
             try:
                 return f(*args, **kwargs)
+            except KeyboardInterrupt:
+                print('\n\nQuitting after teardown...'.format(f.__name__))
+                if with_teardown:
+                    self.handlers['teardown'](self.accumulator)
+                sys.exit(0)
             except:
-                if msg:
-                    print(msg)
-                else:
-                    print('{} failed!'.format(f.__name__))
+                print('{} failed!'.format(f.__name__))
+                if with_teardown:
+                    self.handlers['teardown'](self.accumulator)
                 raise
-            finally:
-                handlers['teardown']()
+
+        return wrapped
+
+    def setup(self, f):
+        wrapped = self.wrap_in_try(f, with_teardown=False)
+        self.handlers['setup'] = wrapped
+        return f
+
+    def newdata(self, f):
+        wrapped = self.wrap_in_try(f)
+        self.handlers['newdata'] = wrapped
+        return f
+
+    def foreach(self, f):
+        wrapped = self.wrap_in_try(f)
+        self.handlers['foreach'] = wrapped
+        return f
+
+    def teardown(self, f):
+        wrapped = self.wrap_in_try(f, with_teardown=False)
+        self.handlers['teardown'] = wrapped
+        return f
+
+    def skipif(self, f):
+        wrapped = self.wrap_in_try(f)
+        self.handlers['skipif'] = wrapped
+        return f
+
+    def describe(self, f):
+        wrapped = self.wrap_in_try(f)
+        self.handlers['decribe'] = wrapped
+        return f
+
+    def run(self):
+        setup = self.handlers['setup']
+        newdata = self.handlers['newdata']
+        foreach = self.handlers['foreach']
+        skipif = self.handlers['skipif']
+        describe = self.handlers['describe']
+        teardown = self.handlers['teardown']
+
+        if setup:
+            self.accumulator = setup()
         else:
-            try:
-                return f(*args, **kwargs)
-            except:
-                if msg:
-                    print(msg)
+            self.accumulator = []
+
+        if newdata:
+            self.new_data = newdata(self.accumulator)
+        else:
+            self.new_data = []
+
+        for item in self.new_data:
+            if skipif and skipif(item):
+                if describe:
+                    print('Skipping {}'.format(describe(item)))
                 else:
-                    print('{} failed!'.format(f.__name__))
-                raise
+                    print('Skipping {}'.format(item))
 
+            print(chr(27) + "[2J")
+            if foreach:
+                foreach(item, self.accumulator)
 
-def setup(f):
-    wrapped = wrap_in_try(f, with_teardown=False)
-    handlers['setup'] = wrapped
-    return f
-
-
-def newdata(f):
-    wrapped = wrap_in_try(f)
-    handlers['newdata'] = wrapped
-    return f
-
-
-def foreach(f):
-    wrapped = wrap_in_try(f)
-    handlers['foreach'] = wrapped
-    return f
-
-
-def teardown(f):
-    wrapped = wrap_in_try(f, with_teardown=False)
-    handlers['teardown'] = wrapped
-    return f
-
-
-def skipif(f):
-    wrapped = wrap_in_try(f)
-    handlers['skipif'] = wrapped
-    return f
-
-
-def describe(f):
-    wrapped = wrap_in_try(f)
-    handlers['decribe'] = wrapped
-    return f
-
-
-def run():
-    setup = handlers['setup']
-    newdata = handlers['newdata']
-    foreach = handlers['foreach']
-    skipif = handlers['skipif']
-    describe = handlers['describe']
-    teardown = handlers['teardown']
-
-    if setup:
-        existing_data = setup()
-    else:
-        existing_data = []
-
-    if newdata:
-        new_data = newdata(existing_data)
-    else:
-        new_data = []
-
-    for item in new_data:
-        if skipif and skipif(item):
-            if describe:
-                print('Skipping {}'.format(describe(item)))
-            else:
-                print('Skipping {}'.format(item))
-
-        print(chr(27) + "[2J")
-        if foreach:
-            foreach(item, existing_data)
-
-    if teardown:
-        teardown(existing_data)
+        if teardown:
+            teardown(self.accumulator)
